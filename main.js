493c94e2f8e69d7efc7194a94731d5862417cb1f
pedrotanak <106190056+pedrotanak@users.noreply.github.com>
2022-11-10 17:13:03 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,4 +8,30 @@ function botao(){
 escuta.onresult=function (evento){
     var conteudo=evento.results[0][0].transcript;
     textbox.innerHTML=conteudo;
+    if (conteudo == "minha selfie") {
+        fala();
+    
+    }
 }
+function fala(){
+    var sintetizador=window.speechSynthesis;
+    guardaFrase="Tirando sua Selfie em 5 segundos";
+    var fale=new SpeechSynthesisUtterance(guardaFrase);
+    sintetizador.speak(fale);
+    Webcam.attach(camera);
+    setTimeout(function(){
+        tirefoto();
+        salvafoto();
+    },5000
+    );    
+    
+}
+var camera=document.getElementById("camera");
+Webcam.set({
+    width:400,
+    height:200,
+    image_format:"jpeg",
+    jpeg_quality:100
+});
+
+
